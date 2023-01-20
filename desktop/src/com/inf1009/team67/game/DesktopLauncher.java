bba04c5390ef9991527e273d1c73fc42760662a7
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-01-20 18:46:07 +0800

    Initial commit

---
+++ desktop/src/com/inf1009/team67/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.inf1009.team67.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.inf1009.team67.game.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("INF1009 Team 67 Group Project");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
