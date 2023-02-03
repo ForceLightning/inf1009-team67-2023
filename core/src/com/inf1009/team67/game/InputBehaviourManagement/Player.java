fb234bf12193f8df94e774778d93e34408d7402c
Thaddaeus <113769099+Thaddaeus-K@users.noreply.github.com>
2023-02-03 15:56:20 +0800

    Player constructor created

---
+++ core/src/com/inf1009/team67/game/InputBehaviourManagement/Player.java
@@ -0,0 +1,9 @@
+package com.inf1009.team67.game.InputBehaviourManagement;
+
+import com.inf1009.team67.game.EntityManagement.Entity;
+
+public class Player extends Entity {
+    public Player(float x, float y, int z, float width, float height, String texturePath, float movementSpeed, int colorHex) {
+        super(x, y, z, width, height, texturePath, movementSpeed, colorHex);
+    }
+}
