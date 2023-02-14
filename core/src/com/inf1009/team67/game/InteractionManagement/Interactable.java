2585e8405c5f15760ebd9749ce6febda448890ea
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-02-14 16:16:47 +0800

    fix: re-added interactable interface

---
+++ core/src/com/inf1009/team67/game/InteractionManagement/Interactable.java
@@ -0,0 +1,11 @@
+package com.inf1009.team67.game.InteractionManagement;
+
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.scenes.scene2d.Action;
+
+public interface Interactable {
+    public void handleInteraction(Interactable other);
+    public boolean isInteractingWith(Interactable other);
+    public Circle getInteractionCircle();
+    public void addReaction(Interactable other, Action reaction);
+}
