7d72a1119097d3948fc4a8c2627d458a97c3bd1c
Christopher Kok <ForceLightning@users.noreply.github.com>
2023-03-17 12:10:47 +0800

    fix: slightly modified enemy and healthpack debug colors for visual feedback

--- core/src/com/inf1009/team67/game/food/UnhealthyFood.java
+++ core/src/com/inf1009/team67/game/food/UnhealthyFood.java
@@ -1,5 +1,8 @@
 package com.inf1009.team67.game.food;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
 public class UnhealthyFood extends HealthPack {
     public UnhealthyFood() {
         this(50, 0.5f, 0.5f);
@@ -9,5 +12,13 @@ public class UnhealthyFood extends HealthPack {
         super(health, movementSpeedAilment, maxHealthAilment);
         this.setTexture("textures/circle.png");
         this.setSize(10, 10);
+    }
+
+    @Override
+    public void drawDebug(ShapeRenderer shapes) {
+        Color oldColor = shapes.getColor();
+        shapes.setColor(Color.RED);
+        super.drawDebug(shapes);
+        shapes.setColor(oldColor);
     }
 }
